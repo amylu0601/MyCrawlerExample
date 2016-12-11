11d977be56a343dd48760ec054c83976c470a533
amylu0601 <amylu0601@gmail.com>
2016-12-11 12:24:59 +0800

    ptt search text practice

---
+++ src/crawler/example/PTTpracticce.java
@@ -0,0 +1,58 @@
+package crawler.example;
+
+import com.github.abola.crawler.CrawlerPack;
+import org.apache.commons.logging.impl.SimpleLog;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+/**
+ * 爬蟲包程式的全貌，就只有這固定的模式
+ * 
+ * @author Abola Lee
+ *
+ */
+public class PTTpracticce {
+	// commit test
+	public static void main(String[] args) {
+
+		// set to debug level
+		//CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_DEBUG);
+
+		// turn off logging
+		CrawlerPack.setLoggerLevel(SimpleLog.LOG_LEVEL_OFF);
+
+		// 遠端資料路徑
+		String uri = "https://www.ptt.cc/bbs/Gossiping/M.1481421663.A.582.html";
+			//	+ "/httpLogin/download.tar.gz"
+			//	+ "!download.tar"
+			//	+ "!/path/data";
+
+		Document jsoup
+				=CrawlerPack.start()
+
+				
+				// 參數設定
+			    .addCookie("over18","1")	// 設定cookie
+				//.setRemoteEncoding("big5")// 設定遠端資料文件編碼
+				
+				// 選擇資料格式 (三選一)
+				//.getFromJson(uri)
+			    .getFromHtml(uri);
+			    //.getFromXml(uri)
+			    
+			    // 這兒開始是 Jsoup Document 物件操作
+			    //.select(".css .selector ")
+
+				//.select(".push-tag:matchesOwn(推) + .push-userid")
+				//.select(".push-userid,.push-content")
+
+				//.select(".push-tag:matchesOwn(推) + .push-userid")
+            for(Element elems:jsoup.select("#main-content *")) {
+
+				//System.out.println(elems.toString());
+                elems.remove();
+
+			}
+			System.out.println(jsoup.select("#main-content ").text());
+	}
+}
